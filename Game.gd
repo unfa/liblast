8b75f4ffa6408e8af0d40ac44c73729abe4ab89b
Jan <heemstrajan@gmail.com>
2020-06-14 20:59:37 +0200

    Fixed network status debugging

--- Game.gd
+++ Game.gd
@@ -6,6 +6,8 @@ export var SERVER_PORT = 9999
 export var SERVER_IP = "172.28.162.191"
 export var MAX_PLAYERS = 10
 
+var player_scene = preload("res://Player.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -36,18 +38,21 @@ func initialize():
 func initialize_server():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(SERVER_PORT, MAX_PLAYERS)
-	peer.connect("connected_to_server", self, "on_connection_established")
-	peer.connect("connection_failed", self, "on_connection_failed")
+	get_tree().connect("network_peer_connected", self, "on_peer_connected")
 	return peer
 
 func initialize_client():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(SERVER_IP, SERVER_PORT)
-	peer.connect("network_peer_connected", self, "on_peer_connected")
+	get_tree().connect("connected_to_server", self, "on_connection_established")
+	get_tree().connect("connection_failed", self, "on_connection_failed")
 	return peer
 
 func on_peer_connected(id):
 	print("Peer connected with id ", id)
+	var player = player_scene.instance()
+	add_child(player)
+	player.set_network_master(id)
 
 func on_connection_established():
 	print("Connection has been established")
