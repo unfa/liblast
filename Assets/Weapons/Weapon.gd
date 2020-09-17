53b32892e2ba8ca3247f5e85c03b2a959c7f446b
unfa <unfa00@gmail.com>
2020-09-18 01:09:54 +0200

    Bullet tracers.

--- Assets/Weapons/Weapon.gd
+++ Assets/Weapons/Weapon.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 var casing = preload("res://Assets/Weapons/Handgun/Casing.tscn")
+var tracer = preload("res://Assets/Effects/BulletTracer.tscn")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -24,10 +25,22 @@ remote func shoot():
 	casing_instance.angular_velocity = Vector3(rand_range(0, 40), 0, 0)
 	casing_instance.linear_velocity = Vector3(rand_range(0, 1), 5, 0)
 	
+	get_tree().root.call_deferred("add_child", casing_instance)
+	
+	var tracer_instance = tracer.instance()
+	tracer_instance.global_transform = find_node("Muzzle").global_transform
+	
+	get_tree().root.call_deferred("add_child", tracer_instance)
+	
+	#tracer_instance.rotate_object_local(Vector3.AXIS_Y, PI/4)
+	#tracer_instance.angular_velocity = Vector3(rand_range(0, 40), 0, 0)
+	#tracer_instance.linear_velocity = Vector3(rand_range(0, 1), 5, 0)
+	
+	
 	# TODO - fix casing rotation
 	# TODO - apply initial linear and angular velocity	
 	
-	get_tree().root.call_deferred("add_child", casing_instance)
+	
 	
 	$Handgun/AnimationPlayer.stop()
 	$Handgun/AnimationPlayer.play("Shoot", -1, 2)
