63b48d45a9805297ad17059eabec7d7d890aac1e
Jan <heemstrajan@gmail.com>
2020-09-30 02:08:16 +0200

    Added weapon reloading

--- Assets/HUD/HUD.gd
+++ Assets/HUD/HUD.gd
@@ -1,10 +1,13 @@
 extends Control
 
+func update_ammo(var weapon, var amount):
+	$Weapon/VBoxContainer/RoundsClips.text = str(amount)
+
 func updateHealth(health: int):
 	$Health/HealthBar.value = health
 	$Health/HealthBar/HealthText.text = String(health)
 	
-func updateCrosshair(visible: bool, hit: bool):
+func update_crosshair(visible: bool, hit: bool):
 	$Crosshair.visible = visible
 	if hit:
 		$Crosshair/HitConfirmation.activate(0.2)
