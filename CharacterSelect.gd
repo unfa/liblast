9005ad2ac7e7f2b037b1b4881580ad5d33e6e9dc
desktop <desktop@jan-desktop.home>
2020-12-10 00:26:43 +0100

    Added disconnect button back

--- CharacterSelect.gd
+++ CharacterSelect.gd
@@ -1,4 +1,12 @@
 extends Control
 
+onready var game = get_parent().get_parent()
+
+func _ready():
+	var dir = Directory.new()
+	dir.open("res://Assets/Characters/")
+	
+	#$CenterContainer/VBoxContainer/CharacterList.add_child(null)
+
 func spawn():
 	get_parent().get_parent().spawn(get_tree().get_network_unique_id())
