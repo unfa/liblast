f4f18a346d997f3c657675fd7826ccd4aa6c48b2
desktop <desktop@jan-desktop.home>
2021-02-27 00:20:51 +0100

    Final fix for character select

--- Assets/Characters/SmartFridge/SmartFridge.gd
+++ Assets/Characters/SmartFridge/SmartFridge.gd
@@ -2,4 +2,6 @@ extends "res://Player.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if name == String(get_tree().get_network_unique_id()):
+		$SmartFridge/Cube.cast_shadow = 3
+		$SmartFridge/Cube001.cast_shadow = 3
