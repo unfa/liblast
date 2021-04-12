054f9ee56a16ad096e20c8ed326b8b230cf17739
Jan Heemstra <heemstrajan@gmail.com>
2021-04-12 02:08:28 +0200

    Fixed bullet tracers

--- Game/Assets/Effects/BulletTracer.gd
+++ Game/Assets/Effects/BulletTracer.gd
@@ -1,24 +1,18 @@
 extends Spatial
 
 var first = true
-var velocity = 4
+var velocity = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	translate_object_local(Vector3(-10,0,0))
+	set_process(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if first:
-		translate_object_local(Vector3(- rand_range(0, velocity) - velocity*3 ,0 ,0))
-		first = false
-	else:
-		translate_object_local(Vector3(-velocity,0,0))
+	translate_object_local(Vector3(-velocity * delta,0,0))
+
+const casing = preload("res://Assets/Weapons/Handgun/Casing.gd")
 
 func _on_Raycast_body_entered(body):
-	#print("Tracer died:", body)
-	queue_free()
+	if not (body is Player) and not (body is casing):
+		queue_free()
