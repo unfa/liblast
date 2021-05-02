09be57ff207908253d07dba4f3517f8b321e54bd
Jan Heemstra <heemstrajan@gmail.com>
2021-05-03 00:49:51 +0200

    Added functioning grenade launcher

---
+++ Game/Assets/Weapons/GrenadeLauncher/GrenadeLauncher.gd
@@ -0,0 +1,28 @@
+extends "res://Classes/Weapon/Weapon.gd"
+
+var grenade = preload("res://Assets/Weapons/GrenadeLauncher/Grenade.tscn")
+
+func set_trigger_held_primary(active:bool):
+	trigger_held_primary = active
+	
+	if active:
+		shoot(camera)
+
+func shoot(camera):
+	if is_reloading:
+		return
+	
+	if currently_fireing == true:
+		cached_fire = true
+		yield($Model/AnimationPlayer, "animation_finished")
+	
+	rpc("fire_weapon")
+
+sync func fire_weapon():
+	var grenade_instance = grenade.instance()
+	var muzzle_transform = $Model/Muzzle.global_transform
+	grenade_instance.global_transform = muzzle_transform
+	
+	grenade_instance.linear_velocity = muzzle_transform.basis.y * 40 + player.velocity
+	
+	get_tree().root.call_deferred("add_child", grenade_instance)
--- Game/Assets/Weapons/Handgun/Handgun.gd
+++ Game/Assets/Weapons/Handgun/Handgun.gd
@@ -4,14 +4,10 @@ func set_trigger_held_primary(active:bool):
 	trigger_held_primary = active
 	
 	if active:
-		shoot(camera, true)
+		shoot(camera)
 	print("trigger_active", active)
 
-func shoot(camera, primary):
-	if not Automatic:
-		if cached_fire == true:
-			return
-	
+func shoot(camera):
 	if is_reloading:
 		return
 	
@@ -62,3 +58,62 @@ func shoot(camera, primary):
 		reload()
 	
 	return current_rounds
+
+sync func fire_weapon(var rounds_left):
+	show_muzzle_flash(rounds_left)
+	show_tracer()
+	spawn_casing()
+	yield($Model/AnimationPlayer, "animation_finished")
+	
+	if !cached_fire:
+		currently_fireing = false
+
+func show_muzzle_flash(var rounds_left):
+	$Model/AnimationPlayer.stop()
+	
+	if rounds_left == 1:
+		$Model/AnimationPlayer.play("Empty", -1, 2)
+	else:
+		$Model/AnimationPlayer.play("Shoot", -1, 2)
+	
+	$Effects/Flash.stop(true)
+	$Effects/Flash.play("Flash")
+	
+	$Effects/MuzzleFlash.emitting = true
+	yield(get_tree().create_timer(0.07),"timeout")
+	$Effects/MuzzleFlash.emitting = false
+	
+	$Sounds/Shoot.play()
+
+func show_tracer():
+	var tracer_instance = tracer.instance()
+	tracer_instance.hide()
+	tracer_instance.global_transform = global_transform
+	tracer_instance.translation = $Model/Muzzle.global_transform.origin
+
+	get_tree().root.call_deferred("add_child", tracer_instance)
+	tracer_instance.call_deferred("show")
+
+func spawn_casing():
+	#while [ true ]:
+		#var casing_instance = load("res://Assets/Weapons/Handgun/Casing.tscn").instance()
+	var casing_instance = casing.instance()
+	casing_instance.global_transform = ejector.global_transform
+	
+	casing_instance.rotate_object_local(Vector3.FORWARD, deg2rad(90))
+
+	casing_instance.angular_velocity = - ejector.global_transform.basis[2] * rand_range(23, 37)
+	casing_instance.linear_velocity = ejector.global_transform.basis[0] * rand_range(3.2, 4.5) - ejector.global_transform.basis[2] * rand_range(2.6, 3.7)
+	
+	get_tree().root.call_deferred("add_child", casing_instance)
+
+remote func compute_bullet_flyby():
+	var local_player = get_tree().root.get_node("Game").local_player
+	var transform = find_node("Muzzle").global_transform
+	
+	var from = global_transform.xform(Vector3())
+	var to = global_transform.xform(Vector3(-1000, 0, 0))
+	
+	if local_player:
+		local_player.on_bullet_flyby(from, to)
+
