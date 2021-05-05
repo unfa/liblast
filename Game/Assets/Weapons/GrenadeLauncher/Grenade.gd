98f59389ef06da6a04a8a90badeb648006a90e31
unfa <unfa00@gmail.com>
2021-05-06 01:32:44 +0200

    Grenade explosion.

--- Game/Assets/Weapons/GrenadeLauncher/Grenade.gd
+++ Game/Assets/Weapons/GrenadeLauncher/Grenade.gd
@@ -1,8 +1,13 @@
 extends RigidBody
 
+var explosion_effect = preload("res://Assets/Effects/GrenadeExplosion.tscn")
+
 func _process(delta):
 	# TODO: synchronize position
 	pass
 
 func explode():
+	var explosion = explosion_effect.instance()
+	explosion.global_transform = global_transform
+	get_tree().root.call_deferred("add_child", explosion)
 	queue_free()
